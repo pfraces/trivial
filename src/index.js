0d1b2c451b0afcad7b699aef59a79e190c4a4174
Pau Fracés <pfraces@gmail.com>
2023-06-20 14:08:15 +0200

    Added organization to global styles

--- src/index.js
+++ src/index.js
@@ -1,11 +1,7 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
-import '@fontsource/roboto/300.css';
-import '@fontsource/roboto/400.css';
-import '@fontsource/roboto/500.css';
-import '@fontsource/roboto/700.css';
-import './index.css';
+import './styles/styles';
 
 const rootElement = document.getElementById('root');
 const root = createRoot(rootElement);
