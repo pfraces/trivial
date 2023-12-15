6456adf176f0a84a7c974a5599ff83f8ffe1b7e1
Pau Fracés <pfraces@gmail.com>
2023-12-15 15:52:21 +0100

    decoupled session widget

--- src/layout/Header/Header.js
+++ src/layout/Header/Header.js
@@ -1,7 +1,6 @@
 import { useState } from 'react';
-import { Link, useNavigate } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import {
-  Avatar,
   Divider,
   Drawer,
   IconButton,
@@ -10,24 +9,19 @@ import {
   ListItemButton,
   ListItemIcon,
   ListItemText,
-  Menu,
-  MenuItem,
 } from '@mui/material';
 import MenuIcon from '@mui/icons-material/Menu';
 import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
 import HomeIcon from '@mui/icons-material/Home';
 import SchoolIcon from '@mui/icons-material/School';
 import QuizIcon from '@mui/icons-material/Quiz';
-import AccountCircleIcon from '@mui/icons-material/AccountCircle';
-import LogoutIcon from '@mui/icons-material/Logout';
 import { useAuth } from 'src/firebase/auth';
+import SessionWidget from 'src/layout/SessionWidget/SessionWidget';
 import './Header.css';
 
 export default function Header() {
-  const navigate = useNavigate();
-  const { user, logout } = useAuth();
+  const { user } = useAuth();
   const [appMenuOpen, setAppMenuOpen] = useState(false);
-  const [accountMenuAnchor, setAccountMenuAnchor] = useState(null);
 
   const onAppMenuOpen = () => {
     setAppMenuOpen(true);
@@ -35,24 +29,6 @@ export default function Header() {
 
   const closeAppMenu = () => {
     setAppMenuOpen(false);
-  };
-
-  const onAccountMenuOpen = (event) => {
-    setAccountMenuAnchor(event.currentTarget);
-  };
-
-  const closeAccountMenu = () => {
-    setAccountMenuAnchor(null);
-  };
-
-  const onProfile = () => {
-    navigate('profile');
-    closeAccountMenu();
-  };
-
-  const onLogout = () => {
-    closeAccountMenu();
-    logout();
   };
 
   return (
@@ -86,48 +62,8 @@ export default function Header() {
         )}
       </div>
 
-      <div className="session">
-        {!user && (
-          <Link to="login" className="link nowrap">
-            Log in
-          </Link>
-        )}
-
-        {user && (
-          <>
-            <IconButton
-              size="large"
-              className="account-menu-toggler"
-              onClick={onAccountMenuOpen}
-              color="inherit"
-            >
-              <Avatar className="avatar" />
-            </IconButton>
-
-            <Menu
-              anchorEl={accountMenuAnchor}
-              open={Boolean(accountMenuAnchor)}
-              onClose={closeAccountMenu}
-              PaperProps={{ className: 'account-menu' }}
-            >
-              <MenuItem onClick={onProfile}>
-                <ListItemIcon>
-                  <AccountCircleIcon fontSize="small" />
-                </ListItemIcon>
-                Profile
-              </MenuItem>
-
-              <Divider />
-
-              <MenuItem onClick={onLogout}>
-                <ListItemIcon>
-                  <LogoutIcon fontSize="small" />
-                </ListItemIcon>
-                Logout
-              </MenuItem>
-            </Menu>
-          </>
-        )}
+      <div className="widgets">
+        <SessionWidget />
       </div>
 
       <Drawer
---
+++ src/layout/SessionWidget/SessionWidget.js
@@ -0,0 +1,86 @@
+import { useState } from 'react';
+import { useNavigate, Link } from 'react-router-dom';
+import {
+  IconButton,
+  Avatar,
+  Menu,
+  MenuItem,
+  ListItemIcon,
+  Divider,
+} from '@mui/material';
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import LogoutIcon from '@mui/icons-material/Logout';
+import { useAuth } from 'src/firebase/auth';
+import './SessionWidget.css';
+
+export default function SessionWidget() {
+  const [sessionMenuAnchor, setSessionMenuAnchor] = useState(null);
+  const navigate = useNavigate();
+  const { user, logout } = useAuth();
+
+  const onSessionMenuOpen = (event) => {
+    setSessionMenuAnchor(event.currentTarget);
+  };
+
+  const closeSessionMenu = () => {
+    setSessionMenuAnchor(null);
+  };
+
+  const onProfile = () => {
+    navigate('profile');
+    closeSessionMenu();
+  };
+
+  const onLogout = () => {
+    closeSessionMenu();
+    logout();
+  };
+
+  return (
+    <div className="SessionWidget">
+      {!user && (
+        <Link to="login" className="link nowrap">
+          Log in
+        </Link>
+      )}
+
+      {user && (
+        <>
+          <IconButton
+            size="large"
+            className="session-menu-toggler"
+            onClick={onSessionMenuOpen}
+            color="inherit"
+          >
+            <Avatar className="avatar" />
+          </IconButton>
+
+          <Menu
+            anchorEl={sessionMenuAnchor}
+            open={Boolean(sessionMenuAnchor)}
+            onClose={closeSessionMenu}
+            slotProps={{
+              paper: { className: 'session-menu' },
+            }}
+          >
+            <MenuItem onClick={onProfile}>
+              <ListItemIcon>
+                <AccountCircleIcon fontSize="small" />
+              </ListItemIcon>
+              Profile
+            </MenuItem>
+
+            <Divider />
+
+            <MenuItem onClick={onLogout}>
+              <ListItemIcon>
+                <LogoutIcon fontSize="small" />
+              </ListItemIcon>
+              Logout
+            </MenuItem>
+          </Menu>
+        </>
+      )}
+    </div>
+  );
+}
