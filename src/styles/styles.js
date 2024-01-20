59a7cd6abbab27f0b3c100a392f8960f565a2a30
Pau Fracés <pfraces@gmail.com>
2024-01-20 18:21:25 +0100

    removed card margins on small devices

--- src/styles/styles.js
+++ src/styles/styles.js
@@ -3,6 +3,6 @@ import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
 import './reset.css';
+import './variables.css';
 import './layout.css';
-import './colors.css';
 import './theme.css';
